c09609d07d31c3dd31f3676fbf0699a67c4e678c
吳峻葳 <evan@wujunweideMacBook-Air.local>
2026-06-15 13:30:26 +0900

    fix safari date limit

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 const dateInput =
     document.getElementById("tourDate");
 
-const today =
-    new Date();
+const today = new Date();
 
-const maxDate =
-    new Date();
+today.setHours(0,0,0,0);
+
+const maxDate = new Date(today);
 
 maxDate.setMonth(
     maxDate.getMonth() + 6
 );
 
+function formatDate(date){
+
+    const year = date.getFullYear();
+
+    const month =
+        String(
+            date.getMonth() + 1
+        ).padStart(2,"0");
+
+    const day =
+        String(
+            date.getDate()
+        ).padStart(2,"0");
+
+    return `${year}-${month}-${day}`;
+
+}
+
 dateInput.min =
-    today.toISOString().split('T')[0];
+    formatDate(today);
 
 dateInput.max =
-    maxDate.toISOString().split('T')[0];
+    formatDate(maxDate);
 
     
 
